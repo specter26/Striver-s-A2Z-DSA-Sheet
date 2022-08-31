ddeffa18a2fa8ebba3905fe0b2e402ad413c9e12
specter26 <aayushvijay02@gmail.com>
2022-08-31 11:58:32 +0530

    Test

--- Pattern/binarypattern.cpp
+++ Pattern/binarypattern.cpp
@@ -10,7 +10,7 @@ int main()
  for(int i=1;i<=n;i++){
     for(int j=1;j<=i;j++){
         if((i+j)%2==0){
-            cout<<1;
+            cout<<"1";
         }
         else{
             cout<<0;
